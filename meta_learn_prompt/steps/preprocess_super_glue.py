3d21838bf52ab0c9a83de50c222917afbcae799f
Zhaofeng Wu <zhaofengw@allenai.org>
2021-11-04 11:41:55 -0700

    comment

--- meta_learn_prompt/steps/preprocess_super_glue.py
+++ meta_learn_prompt/steps/preprocess_super_glue.py
@@ -25,21 +25,22 @@ class PreprocessSuperGlue(Step):
 
     def map(self, task: str, split: str, examples: dict[str, list[Any]]) -> dict[str, list[Any]]:
         if task == "copa" and split == "train":
-            # mirror choice1 and choice2
+            # data augmentation: mirror choice1 and choice2
             for field in set(examples.keys()) - {"choice1", "choice2"}:
                 examples[field].extend([x for x in examples[field]])
             examples["choice1"].extend([x for x in examples["choice2"]])
             examples["choice2"].extend([x for x in examples["choice1"]])
         elif task == "wic" and split == "train":
-            # mirror sentence1 and sentence2
+            # data augmentation: mirror sentence1 and sentence2
             for field in set(examples.keys()) - {"sentence1", "sentence2"}:
                 examples[field].extend([x for x in examples[field]])
             examples["sentence1"].extend([x for x in examples["sentence2"]])
             examples["sentence2"].extend([x for x in examples["sentence1"]])
         elif task == "record":
-            # we follow the GPT-3 paper wrt @highlight annotations]
+            # we follow the GPT-3 paper wrt @highlight annotations
             examples["passage"] = [p.replace("@highlight\n", "- ") for p in examples["passage"]]
             if split == "train":
+                # create one example per answer
                 new_examples = {k if k != "answers" else "answer": [] for k in examples.keys()}
                 for i, answers in examples["answers"]:
                     for answer in answers:
