56bc357d1305f76128ba64aa1eb8ff48b738d6f3
epwalsh <epwalsh10@gmail.com>
2021-11-11 09:45:46 -0800

    improve test

--- tests/data/data_utils_test.py
+++ tests/data/data_utils_test.py
@@ -1,24 +1,21 @@
+from itertools import islice
 import random
-
-import pytest
 
 from meta_learn_prompt.data.data_utils import MixerStreamDataset
 
 
-@pytest.mark.parametrize("seed", [0, 1, 2, 3, 4, 5])
-def test_mixer_stream_dataset(seed):
-    random.seed(seed)
+def test_mixer_stream_dataset():
+    random.seed(1)
 
     dataset1 = ["a", "b", "c"]
     dataset2 = ["1", "2", "3", "4"]
     mixer = MixerStreamDataset([dataset1, dataset2], [0.1, 0.9])
     counts: dict[str, int] = {"dataset1": 0, "dataset2": 0}
-    for i, x in enumerate(iter(mixer)):
+    for i, x in enumerate(islice(iter(mixer), 300)):
         assert isinstance(x, str)
         if x in dataset1:
             counts["dataset1"] += 1
         else:
             counts["dataset2"] += 1
-        if i > 20:
-            break
-    assert counts["dataset2"] > counts["dataset1"]
+    # Make sure the ratio of items sampled from dataset2 compared to dataset1 is about 9-to-1.
+    assert 8.5 < counts["dataset2"] / counts["dataset1"] < 9.5
