cf81aa05d2c8d8119bfd17f382164a8f8bf9155f
Zhaofeng Wu <zhaofengw@allenai.org>
2021-11-12 11:36:58 -0800

    ancient python doesn't have dict concat

--- meta_learn_prompt/data/data_utils.py
+++ meta_learn_prompt/data/data_utils.py
@@ -89,10 +89,14 @@ def collate_fn(
     batch = [{k: np.array(v) for k, v in e.items()} for e in batch]
     max_shapes = _find_max_shapes(batch, pad_token_map.keys())
     for i, e in enumerate(batch):
-        batch[i] = {label_key: e[label_key]} | {
-            k: _pad(e[k], pad_token, max_shapes[k] - np.array(e[k].shape), padding_side)
-            for k, pad_token in pad_token_map.items()
-        }  # dict concatenation overrides label_key if present
+        updated = {label_key: e[label_key]}
+        updated.update(
+            {
+                k: _pad(e[k], pad_token, max_shapes[k] - np.array(e[k].shape), padding_side)
+                for k, pad_token in pad_token_map.items()
+            }
+        )  # overrides label_key if present
+        batch[i] = updated
         batch[i] = {k: _tensorize(v, k, output_mode, label_key) for k, v in batch[i].items()}
     return default_collate(batch)
 
