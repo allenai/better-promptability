f8beae36da6349cee63a38ff2b44b3ddcbff56c8
epwalsh <epwalsh10@gmail.com>
2021-12-02 10:11:08 -0800

    download in parallel

--- scripts/download_t0_training_set.py
+++ scripts/download_t0_training_set.py
@@ -1,29 +1,52 @@
+"""
+Download all of the data from the [bigscience/P3](https://huggingface.co/datasets/bigscience/P3)
+corresponding to a particular mixture. This script should only be run from the root of this repository.
+"""
+
+import asyncio
 import os
 import sys
+from typing import Optional, Tuple
 
 import datasets
-from tqdm import tqdm
 
 
-def main(mixture_name: str, cache_dir: str):
+async def run(cmd: str) -> Tuple[int, str, str]:
+    proc = await asyncio.create_subprocess_shell(
+        cmd, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE
+    )
+
+    stdout, stderr = await proc.communicate()
+    assert proc.returncode is not None
+    return proc.returncode, stdout.decode() if stdout else "", stderr.decode() if stderr else ""
+
+
+async def main(mixture_name: str, cache_dir: str, task: Optional[str] = None):
+    def download_task_dataset(task_name: str):
+        local_path = os.path.join(cache_dir, task_name)
+        if not os.path.isdir(local_path) or not os.listdir(local_path):
+            dataset = datasets.load_dataset("bigscience/P3", task_name, cache_dir=cache_dir)
+            dataset.save_to_disk(local_path)
+
     tasks = [
         line.strip()
         for line in open(f"data/{mixture_name}_tasks.txt")
         if not line.startswith("story_cloze_")  # these are handled separately
     ]
-    # I tried to parallelize this but the loading script for this dataset uses multiprocessing
-    # itself, which causes errors when we try to use a multiprocessing Pool. There is probably
-    # a way around this, but it didn't seem worth anymore development time at the moment since
-    # we probably won't have to run this script more than once.
-    with tqdm(tasks) as task_iter:
-        for task_name in task_iter:
-            task_iter.set_postfix({"task": task_name})
-            local_path = os.path.join(cache_dir, task_name)
-            if os.path.isdir(local_path) and os.listdir(local_path):
-                continue
-            dataset = datasets.load_dataset("bigscience/P3", task_name, cache_dir=cache_dir)
-            dataset.save_to_disk(local_path)
+
+    if task is not None:
+        assert task in tasks
+        download_task_dataset(task)
+    else:
+        await asyncio.gather(
+            *[
+                run(
+                    f"python scripts/download_t0_training_set.py '{mixture_name}' '{cache_dir}' '{task}'"
+                )
+                for task_name in tasks
+            ]
+        )
 
 
 if __name__ == "__main__":
-    main(*sys.argv[1:])
+    asyncio.run(main(*sys.argv[1:]))
