1bd0e2dd58ec779ef8b00335ce10a4d70fcd9cfa
epwalsh <epwalsh10@gmail.com>
2021-12-13 17:52:06 -0800

    rename 'max_size' -> 'sampling_cap'

--- meta_learn_prompt/data/data_utils.py
+++ meta_learn_prompt/data/data_utils.py
@@ -99,17 +99,17 @@ class MixerDataset(Sequence[T]):
     """
     This dataset mixes multiple other datasets into a single :class:`Dataset`.
 
-    The `max_size` argument sets an artificial size limit for all of the datasets which
+    The `sampling_cap` argument sets an artificial size limit for all of the datasets which
     controls the sampling probability for each. This is useful when you have a mix of small
-    and large datasets. When using `max_size`, you should call :meth:`resample()` periodically
-    to randomize the examples that get picked from the undersampled datasets, i.e. the datasets
-    that are bigger than `max_size`.
+    and large datasets. When using `sampling_cap`, you should call :meth:`resample()` after every
+    epoch to randomize the examples that get picked from the undersampled datasets, i.e. the datasets
+    that are bigger than `sampling_cap`.
     """
 
     def __init__(
         self,
         datasets: list[Sequence[T]],
-        max_size: Optional[int] = None,
+        sampling_cap: Optional[int] = None,
         seed: Optional[int] = None,
     ):
         self._datasets: list[Sequence[T]] = []
@@ -117,10 +117,10 @@ class MixerDataset(Sequence[T]):
         self._dataset_boundaries: list[tuple[int, int]] = []
         for dataset in datasets:
             start_boundary = 0 if not self._dataset_boundaries else self._dataset_boundaries[-1][-1]
-            if max_size is not None and len(dataset) > max_size:
-                self._total_size += max_size
-                self._dataset_boundaries.append((start_boundary, start_boundary + max_size))
-                self._datasets.append(_UndersampledDataset(dataset, max_size, seed=seed))
+            if sampling_cap is not None and len(dataset) > sampling_cap:
+                self._total_size += sampling_cap
+                self._dataset_boundaries.append((start_boundary, start_boundary + sampling_cap))
+                self._datasets.append(_UndersampledDataset(dataset, sampling_cap, seed=seed))
             else:
                 self._total_size += len(dataset)
                 self._dataset_boundaries.append((start_boundary, start_boundary + len(dataset)))
@@ -142,55 +142,57 @@ class MixerDataset(Sequence[T]):
 
 
 class _UndersampledDataset(Sequence[T]):
-    def __init__(self, dataset: Sequence[T], max_size: int, seed: Optional[int] = None):
-        assert max_size < len(dataset)
+    def __init__(self, dataset: Sequence[T], sampling_cap: int, seed: Optional[int] = None):
+        assert sampling_cap < len(dataset)
         self._dataset = dataset
-        self._max_size = max_size
+        self._sampling_cap = sampling_cap
         self._indices = list(range(len(self._dataset)))
-        self._max_taken = max_size
+        self._max_taken = sampling_cap
         if seed is not None:
             random.seed(seed)
         #  random.shuffle(self._indices)
 
     def __getitem__(self, i: int) -> T:  # type: ignore[override]
-        if i > self._max_size:
+        if i > self._sampling_cap:
             raise IndexError("index out of bounds")
         return self._dataset[self._indices[i]]
 
     def __len__(self) -> int:
-        return self._max_size
+        return self._sampling_cap
 
     def resample(self, seed: Optional[int]):
         if seed is not None:
             random.seed(seed)
-        if self._max_taken + self._max_size <= len(self._dataset):
+        if self._max_taken + self._sampling_cap <= len(self._dataset):
             # Re-organize `self._indices` so that the latest used chunk is pulled off and put on the end.
-            self._indices = self._indices[self._max_size :] + self._indices[: self._max_size]
-            self._max_taken += self._max_size
+            self._indices = (
+                self._indices[self._sampling_cap :] + self._indices[: self._sampling_cap]
+            )
+            self._max_taken += self._sampling_cap
         else:
             # Re-shuffle `self._indices` in a way that ensures the last chunk we have got to is
             # used next.
             used = (
-                self._indices[: self._max_size]
-                + self._indices[self._max_size + (len(self._dataset) - self._max_taken) :]
+                self._indices[: self._sampling_cap]
+                + self._indices[self._sampling_cap + (len(self._dataset) - self._max_taken) :]
             )
             unused = self._indices[
-                self._max_size : self._max_size + (len(self._dataset) - self._max_taken)
+                self._sampling_cap : self._sampling_cap + (len(self._dataset) - self._max_taken)
             ]
             # `used` will be sliced up and moved around before being added back into `self._indices`,
             # so we shuffle it now to add randomness.
             random.shuffle(used)
 
-            # `next_up` is the next chunk of `self._max_size` which will include all
+            # `next_up` is the next chunk of `self._sampling_cap` which will include all
             # of `unused` and however many examples from `used` that we need to reach
-            # `self._max_size` instances.
-            next_up = unused + used[: self._max_size - len(unused)]
+            # `self._sampling_cap` instances.
+            next_up = unused + used[: self._sampling_cap - len(unused)]
             random.shuffle(next_up)
 
             # Put everything back together into `self._indices`.
-            self._indices = next_up + used[self._max_size - len(unused) :]
+            self._indices = next_up + used[self._sampling_cap - len(unused) :]
 
             # clean up to hopefully help GC
             del used, unused, next_up
 
-            self._max_taken = self._max_size
+            self._max_taken = self._sampling_cap
