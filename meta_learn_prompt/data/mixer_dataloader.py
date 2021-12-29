d8939e36733d2e2cafb50276f46cf70c90d8842c
Zhaofeng Wu <zhaofengw@allenai.org>
2021-12-28 22:53:49 -0800

    Meta learning data fixes/improvements

--- meta_learn_prompt/data/mixer_dataloader.py
+++ meta_learn_prompt/data/mixer_dataloader.py
@@ -1,30 +1,52 @@
 from __future__ import annotations
 
+import math
 import random
+from typing import Callable
 
 from torch.utils.data.dataloader import DataLoader, _BaseDataLoaderIter
 
 
 class MixerDataLoader(DataLoader):
-    def __init__(self, dataloaders: list[DataLoader], meta_batch_size: int):
-        self._dataloaders = dataloaders
+    """
+    A dataloader that encapsulates multiple dataloaders. At each iteration, yields the next batch
+    from a random dataloader.
+    """
+
+    def __init__(
+        self,
+        dataloaders: list[DataLoader],
+        meta_batch_size: int,
+        batch_postprocessor: Callable[[list], list] = lambda b: b,
+    ):
+        self._dataloader_iters = [iter(dataloader) for dataloader in dataloaders]
         self._meta_batch_size = meta_batch_size
-        self._total_len = sum(len(dataloader) for dataloader in dataloaders) // meta_batch_size
+        self._batch_postprocessor = batch_postprocessor
+        self._total_len = int(
+            math.ceil(sum(len(dataloader) for dataloader in dataloaders) / meta_batch_size)
+        )
         self._weights = [len(dataloader) for dataloader in dataloaders]
 
+        self.num_workers = 0  # TODO: multiprocessing
+        self.collate_fn = None
+        self.dataset = None
+
     def __iter__(self) -> _BaseDataLoaderIter:
-        batches = []
-        for _ in range(self._meta_batch_size):
-            assert all(w >= 0 for w in self._weights)
-            if all(w == 0 for w in self._weights):
-                break
-            # TODO: this might not be robust in distributed training
-            dataloader_idx = random.choices(range(len(self._dataloaders)), self._weights)[0]
-            self._weights[dataloader_idx] -= 1
-            dataloader = self._dataloaders[dataloader_idx]
-            batches.append(next(dataloader))
-        assert len(batches) > 0
-        yield batches
+        while True:
+            batches = []
+            for _ in range(self._meta_batch_size):
+                # TODO: this might not be robust in distributed training
+                dataloader_idx = random.choices(range(len(self._dataloader_iters)), self._weights)[
+                    0
+                ]
+                self._weights[dataloader_idx] -= 1
+                dataloader_iter = self._dataloader_iters[dataloader_idx]
+                batches.append(next(dataloader_iter))
+                assert all(w >= 0 for w in self._weights)
+                if all(w == 0 for w in self._weights):
+                    return
+            assert len(batches) > 0
+            yield self._batch_postprocessor(batches)
 
     def __len__(self) -> int:
         return self._total_len
--- meta_learn_prompt/data/t0_meta_learning_data_module.py
+++ meta_learn_prompt/data/t0_meta_learning_data_module.py
@@ -1,15 +1,35 @@
 from __future__ import annotations
+import random
 from typing import Optional
 
-from tango.common import PathOrStr
+from datasets import Dataset as HFDataset
+from tango.common import PathOrStr, Tqdm
 from torch.utils.data.dataloader import DataLoader
-from transformers.trainer_pt_utils import LengthGroupedSampler, DistributedLengthGroupedSampler
+from transformers.trainer_pt_utils import LengthGroupedSampler
 
 from .config import Config
 from .data_utils import collate_fn
 from .mixer_dataloader import MixerDataLoader
+from .mixer_dataset import _UndersampledDataset
 from .prompt_data_module import PromptDataModule
 from .t0_multitask_data_module import T0MultiTaskDataModule
+
+
+def split_batch(meta_batch: list, support_batch_size: int) -> list:
+    # Because each batch is internally sorted by length, the naive split will cause a distributional
+    # difference.
+    processed_meta_batch = []
+    for batch in meta_batch:
+        batch_size = len(list(batch.values())[0])
+        assert all(len(v) == batch_size for v in batch.values())
+        support_indices = random.sample(range(batch_size), support_batch_size)
+        support_indices_set = set(support_indices)
+        query_indices = [i for i in range(batch_size) if i not in support_indices_set]
+
+        support_batch = {k: v[support_indices] for k, v in batch.items()}
+        query_batch = {k: v[query_indices] for k, v in batch.items()}
+        processed_meta_batch.append((support_batch, query_batch))
+    return processed_meta_batch
 
 
 @PromptDataModule.register("t0_meta_learning")
@@ -17,6 +37,7 @@ class T0MetaLearningDataModule(T0MultiTaskDataModule):
     def __init__(
         self,
         meta_batch_size: int,
+        support_batch_size: int,
         mixture_name: str,  # should be 'd4_train', 'd4_dev', or 'green'.
         config: Config,
         num_prefix: int,
@@ -25,6 +46,7 @@ class T0MetaLearningDataModule(T0MultiTaskDataModule):
         **kwargs
     ):
         self.meta_batch_size = meta_batch_size
+        self.support_batch_size = support_batch_size
         super().__init__(
             mixture_name, config, num_prefix, transformer_model, sampling_cap=sampling_cap, **kwargs
         )
@@ -35,29 +57,40 @@ class T0MetaLearningDataModule(T0MultiTaskDataModule):
         assert all(pad is not None for pad in pad_token_map.values())
 
         dataloaders = []
-        for dataset in dataset_split._datasets:
+        for dataset in Tqdm.tqdm(dataset_split._datasets, desc="Creating dataloaders"):
             # zhaofeng: I don't particularly like this design because of the redundancy with
             # DataModule. But this is necessary at least to accomodate _UndersampledDataset at the
             # moment, unless we can somehow turn it into a DataModule too.
-            if shuffle:
-                # LengthGroupedSampler sorts from longest to shortest; we want the reverse
-                lens = [-len(ids) for ids in dataset[self.sort_key]]
-                if self.config.gpus is None or self.config.gpus <= 1:
-                    sampler = LengthGroupedSampler(batch_size, lengths=lens)
-                else:
-                    sampler = DistributedLengthGroupedSampler(batch_size, lengths=lens)
+            assert shuffle
+            if isinstance(dataset, HFDataset):
+                lens = dataset["sort_key_len"]
+            elif isinstance(dataset, _UndersampledDataset):
+                lens = dataset.get_active_example_lens()
             else:
-                sampler = None
+                assert False
+            # LengthGroupedSampler sorts from longest to shortest; we want the reverse
+            lens = [-l for l in lens]  # noqa: E741
+            if self.config.gpus is None or self.config.gpus <= 1:
+                sampler = LengthGroupedSampler(batch_size, lengths=lens)
+            else:
+                # I haven't verified the sampling logic in the distributed mode
+                raise NotImplementedError
             dataloader = DataLoader(
                 dataset,
                 batch_size=batch_size,
                 shuffle=False,
-                samlper=sampler,
+                sampler=sampler,
+                num_workers=0,  # otherwise too many open files are opened
                 collate_fn=lambda batch: collate_fn(
                     batch, pad_token_map, self.tokenizer.padding_side
                 ),
                 pin_memory=True,
+                drop_last=True,  # division into support/query is unclear with incomplete batches
             )
             dataloaders.append(dataloader)
 
-        return MixerDataLoader(dataloaders, self.meta_batch_size)
+        return MixerDataLoader(
+            dataloaders,
+            self.meta_batch_size,
+            batch_postprocessor=lambda b: split_batch(b, self.support_batch_size),
+        )
