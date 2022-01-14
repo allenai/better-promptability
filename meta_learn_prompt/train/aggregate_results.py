c7f65344a99e2df4575f9f61ffb0331f2c75a811
epwalsh <epwalsh10@gmail.com>
2022-01-13 16:57:09 -0800

    fix type hint

--- meta_learn_prompt/train/aggregate_results.py
+++ meta_learn_prompt/train/aggregate_results.py
@@ -1,4 +1,4 @@
-from typing import Any, Dict, List
+from typing import Any, Dict, List, Tuple
 
 from tango import Format, JsonFormat, Step
 
@@ -10,7 +10,7 @@ class AggregateResults(Step):
     FORMAT: Format = JsonFormat()
     VERSION = "002"
 
-    def run(self, results: List[List[Dict[str, Any]]]) -> Dict[str, Any]:
+    def run(self, results: List[Tuple[str, List[Dict[str, Any]]]]) -> Dict[str, Any]:
         accuracy_total = 0.0
         for _, epoch_metrics in results:
             accuracy_total += epoch_metrics[-1]["best_categorical_accuracy"]
