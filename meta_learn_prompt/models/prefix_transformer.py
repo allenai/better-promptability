c33cb32b91cd191932a98f4cd831dedfc04c4285
Zhaofeng Wu <zhaofengw@allenai.org>
2022-01-15 22:44:04 -0800

    fix

--- meta_learn_prompt/models/prefix_transformer.py
+++ meta_learn_prompt/models/prefix_transformer.py
@@ -151,7 +151,7 @@ class PrefixTransformer(Model):
         new_state_dict = {}
         for k, v in checkpoint["state_dict"].items():
             if k.startswith("model."):
-                new_state_dict[len("model."):] = v
+                new_state_dict[k[len("model."):]] = v
         checkpoint["state_dict"] = new_state_dict
         # TODO: optimizer states
         return super().on_load_checkpoint(checkpoint)
