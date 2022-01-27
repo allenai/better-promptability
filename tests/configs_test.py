42a3c3cd0e3ae323b0becb64264987767c9dab74
Zhaofeng Wu <zhaofengw@allenai.org>
2022-01-26 17:11:36 -0800

    tests

--- tests/configs_test.py
+++ tests/configs_test.py
@@ -1,7 +1,11 @@
+import os
+
 from tango.common import Params
 
 
 def test_few_shot_baseline_all():
+    os.environ["CKPT"] = "null"
     d = Params.from_file("configs/fewshot_baseline_all_green.jsonnet").as_dict()
+    del os.environ["CKPT"]
     assert "result_anli_GPT_3_style_r1_score_eval" in d["steps"]
     assert "aggregated_results" in d["steps"]
