3b34ed370df1ff03d0792166d9e80e39cfe9c472
Dirk Groeneveld <dirkg@allenai.org>
2022-04-05 10:43:23 -0700

    Revert "Can't use a sampler at the same time"
    This reverts commit ad21ff1ed752848f81390d037efe8ce754a2b8fb.

--- meta_learn_prompt/data/data_module.py
+++ meta_learn_prompt/data/data_module.py
@@ -38,7 +38,7 @@ class DataModule(LightningDataModule, DetHashWithVersion):
     and a tokenizer.
     """
 
-    VERSION = "003"
+    VERSION = "002"
 
     def __init__(
         self,
@@ -188,18 +188,15 @@ class DataModule(LightningDataModule, DetHashWithVersion):
         dataset_split = self.dataset_dict[split]
 
         # LengthGroupedSampler sorts from longest to shortest; we want the reverse
-        if shuffle:
-            sampler = None
+        if isinstance(dataset_split, MixerDataset):
+            # The naive processing is slow and takes too much memory
+            lens = [-l for l in dataset_split.get_all_example_lens()]  # noqa: E741
         else:
-            if isinstance(dataset_split, MixerDataset):
-                # The naive processing is slow and takes too much memory
-                lens = [-l for l in dataset_split.get_all_example_lens()]  # noqa: E741
-            else:
-                lens = [-len(ids) for ids in dataset_split[self.sort_key]]
-            if self.config.gpus is None or self.config.gpus <= 1:
-                sampler = LengthGroupedSampler(batch_size, lengths=lens)
-            else:
-                sampler = DistributedLengthGroupedSampler(batch_size, lengths=lens)
+            lens = [-len(ids) for ids in dataset_split[self.sort_key]]
+        if self.config.gpus is None or self.config.gpus <= 1:
+            sampler = LengthGroupedSampler(batch_size, lengths=lens)
+        else:
+            sampler = DistributedLengthGroupedSampler(batch_size, lengths=lens)
 
         pad_token_map = self.pad_token_map(split)
         assert all(pad is not None for pad in pad_token_map.values())
